e513027f44ebef669dcbc52a5bab1a510561c2fd
johnstonpc <102391762+johnstonpc@users.noreply.github.com>
2023-10-19 01:28:27 -0400

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,350 @@
+var portuguesLista = [
+    'Cuidado ao dirigir',
+    'Você pode me traduzir isso?		',
+    'Chicagoé bem diferente de Boston.		',
+    'Não se preocupe.		',
+    'Todo mundo sabe isso.		',
+    'Está tudo pronto.		',
+    'Excelente.		',
+    'De tempos em tempos		',
+    'Boa ideia		',
+    'Ele gosta muito.		',
+    'Socorro!		',
+    'Ele volta logo		',
+    'Ele está certo.		',
+    'Ele está aborrecido.		',
+    'Ele é muito famoso.		',
+    'Como vai você?		',
+    'Como vai o trabalho?		',
+    'Depressa!		',
+    'Eu já comí.		',
+    'Eu não te ouço.		',
+    'Eu gostaria de ir caminhar.		',
+    'Eu não sei como usar isto.		',
+    'Eu não gosto dele.		',
+    'Eu não gosto.		',
+    'Eu não falo muito bem.		',
+    'Eu não entendo.		',
+    'Eu não quero.		',
+    'Eu não quero isso.		',
+    'Eu não quero te incomodar.		',
+    'Eu me sinto bem.		',
+    'Se você precisa de minha ajuda, por favor me diga.		',
+    'Eu saio do trabalho às 6.		',
+    'Eu tenho dor de cabeça.		',
+    'Eu espero que você e tua esposa tenham uma boa viagem.		',
+    'Eu sei.		',
+    'Eu gosto dela.		',
+    'Eu te ligo quando sair.		',
+    'Eu volto mais tarde.		',
+    'Eu pagarei.		',
+    'Eu levo/Eu fico (com alguma coisa)		',
+    'Eu te levo ao ponto de ônibus.		',
+    'Eu perdí meu relógio.		',
+    'Eu amo você.		',
+    'Eu sou um americano.		',
+    'Eu estou limpando meu quarto.		',
+    'Eu estou com frio.		',
+    'Eu estou indo te buscar		',
+    'Eu vou sair.		',
+    'Eu estou bem, e você?		',
+    'Estou feliz.		',
+    'Estou com fome.		',
+    'Sou casado.		',
+    'Eu não estou ocupado.		',
+    'Eu não sou casado.		',
+    'Eu ainda não estou pronto.		',
+    'Eu não tenho certeza.		',
+    'Lamento, está esgotado.		',
+    'Estou com sede.		',
+    'Estou muito ocupado. Não tenho tempo agora.		',
+    'Eu preciso trocar de roupas.		',
+    'Eu preciso ir pra casa.		',
+    'Eu só quero um lanche.		',
+    'O Sr. Smith é americano?		',
+    'É o suficiente?		',
+    'Eu acho isso muito bom		',
+    'Eu acho isso bem saboroso.		',
+    'Eu pensei que as roupas eram mais baratas.		',
+    'É mais longe que 2 milhas.		',
+    'Eu estou aqui há dois dias.		',
+    'Eu ouví que o Texas é um belo lugar.		',
+    'Eu nunca ví isso antes.		',
+    'Eu estava saindo do restaurante quando meus amigos chegaram.		',
+    'Só um pouquinho.		',
+    'Só um momento.		',
+    'Deixa-me ver/checar.		',
+    'Deixa-me pensar sobre isso.		',
+    'Vamos dar uma olhada.		',
+    'Vamos praticar inglês.		',
+    'Posso falar com a Sra. Smith por favor?		',
+    'Mais do que isso.		',
+    'Não faz mal/Não importa.		',
+    'Da próxima vez.		',
+    'Não.		',
+    'Bobagem/Que nada.		',
+    'Não, obrigado/a.		',
+    'Nada mais/Mais nada		',
+    'Não recentemente.		',
+    'Não ainda/Ainda não.		',
+    'Claro.		',
+    'OK.		',
+    'Por favor, preencha este formulário.		',
+    'Por favor me leve para este endereço		',
+    'Por favor escreva aqui.		',
+    'Mesmo?/De verdade?		',
+    'Bem aqui.		',
+    'Bem alí.		',
+    'Te vejo mais tarde.		',
+    'Te vejo amanhã.		',
+    'Te vejo à noite.		',
+    'Ela é bonita.		',
+    'Me perdoe te importunar.		',
+    'Pare!		',
+    'Arrisque.		',
+    'Leve pra fora.		',
+    'Diga-me.		',
+    'Obrigado/a por tudo.		',
+    'Obrigado/a pela tua ajuda		',
+    'Obrigado/a.		',
+    'Obrigado/a senhorita.		',
+    'Obrigado/a senhor.		',
+    'Muito obrigado/a.		',
+    'Está ótimo (aparência)		',
+    'Tudo bem.		',
+    'Chega (é o suficiente)		',
+    'Está bom.		',
+    'É isso.		',
+    'Isso cheira ruim.		',
+    'Isso não é justo		',
+    'Isso não está certo.		',
+    'Está certo.		',
+    'Isso é muito mal/ruim.		',
+    'Isso é muito (quantidade)		',
+    'Isso é muito (quantidade)		',
+    'O livro está debaixo da mesa.		',
+    'Eles voltam logo.		',
+    'Eles são os mesmos.		',
+    'Eles estão bem ocupados.		',
+    'Isto não funciona.		',
+    'Isto é muito difícil.		',
+    'Isto é muito importante.		',
+    'Prove.		',
+    'Muito bem, obrigado/a.		',
+    'Nós gostamos muito.		',
+    'Você anotaria a mensagem por favor?		',
+    'Sim, de verdade.		',
+    'Você é bonito/a		',
+    'Você é muito gentil/legal.		',
+    'Você é bem esperto.		',
+    'Tuas coisas estão todas aqui.',]
+
+
+var inglesLista = [
+    'Drive carefully',
+    'Can you translate this for me?',
+    'Chicago is very different from Boston.',
+    'Dont worry.',
+    'Everyone knows this.',
+    'Everything is ready.',
+    'Great.',
+    'From time to time',
+    'Good idea',
+    'He likes it a lot.',
+    'Help!',
+    'He will be back soon',
+    'He is right.',
+    'He is upset.',
+    'He is very famous.',
+    'How are you doing?',
+    'How is the job?',
+    'Quickly!',
+    'I already ate.',
+    'I can not hear you.',
+    'I would like to go walking.',
+    'I dont know how to use this.',
+    'I do not like him.',
+    'I dont like.',
+    'I do not speak very well.',
+    'I dont understand.',
+    'I dont want.',
+    'I do not want it.',
+    'I do not want to bother you.',
+    'I feel good.',
+    'If you need my help, please tell me.',
+    'I leave work at 6.',
+    'I have a headache.',
+    'I hope you and your wife have a good trip.',
+    'I know.',
+    'I like her.',
+    'I will call you when I leave.',
+    'I will be back later.',
+    'I will pay.',
+    'I take/I stay (with something)',
+    'Iwill take you to the bus stop.',
+    'I lost my watch.',
+    'I love you.',
+    'I am an American.',
+    'Im cleaning my room.',
+    'Im cold.',
+    'Im coming to get you',
+    'Ill leave.',
+    'I am well and you?',
+    'I am happy.',
+    'I am hungry.',
+    'I am married.',
+    'I am not busy.',
+    'Im not married.',
+    'Im not ready yet.',
+    'Im not sure.',
+    'Sorry, its sold out.',
+    'Im thirsty.',
+    'I am very busy. I do not have time now.',
+    'I need to change my clothes.',
+    'I need to go home.',
+    'I just want a snack.',
+    'Is Mr. Smith American?',
+    'That is enough?',
+    'I think this is very good',
+    'I find this very tasty.',
+    'I thought the clothes were cheaper.',
+    'Its farther than 2 miles.',
+    'I heve been here for two days.',
+    'I hear Texas is a beautiful place.',
+    'I heve never seen this before.',
+    'I was leaving the restaurant when my friends arrived.',
+    'Just a little.',
+    'Only a moment.',
+    'Let me see/check.',
+    'Let me think about it.',
+    'Let is take a look.',
+    'Let is practice English.',
+    'May I speak to Mrs. Smith please?',
+    'More than that.',
+    'It doesnt hurt/It doesnt matter.',
+    'The next time.',
+    'No.',
+    'Nonsense/Nothing.',
+    'No, thank you.',
+    'Nothing else/Nothing else',
+    'Not recently.',
+    'Not yet/Not yet.',
+    'Clear.',
+    'OK.',
+    'Please fill in this form.',
+    'Please take me to this address',
+    'Please write here.',
+    'Really?/Really?',
+    'Right here.',
+    'Right there.',
+    'See you later.',
+    'See you tomorrow.',
+    'See you at night.',
+    'She is beautiful.',
+    'Forgive me for bothering you.',
+    'Stop!',
+    'Take a shot.',
+    'Take it outside.',
+    'Tell me.',
+    'Thank you for everything.',
+    'Thank you for your help',
+    'Required to.',
+    'Thank you, miss.',
+    'Thank you sir.',
+    'Thank you very much.',
+    'Its great (appearance)',
+    'All good.',
+    'Enough (enough)',
+    'It is good.',
+    'And that.',
+    'This smells bad.',
+    'Thats not fair',
+    'This is not right.',
+    'It is true.',
+    'This is very bad/bad.',
+    'That is a lot (amount)',
+    'That is a lot (amount)',
+    'The book is under the table.',
+    'They will be back soon.',
+    'They are the same.',
+    'They are very busy.',
+    'This does not work.',
+    'This is very hard.',
+    'This is very important.',
+    'Taste it.',
+    'Very well, thank you.',
+    'We like a lot.',
+    'Would you take down the message please?',
+    'Yes, really.',
+    'You are beautiful',
+    'You are very kind/nice.',
+    'You are very smart.',
+    'Your things are all here.',]
+
+var intervalo;
+var contador = 0;
+
+
+
+
+
+
+
+
+
+function lista() {
+
+    var p_portugues = document.querySelector('.portugues')
+    var p_ingles = document.querySelector('.ingles')
+
+    p_portugues.innerHTML = portuguesLista[contador]
+    p_ingles.innerHTML = inglesLista[contador]
+
+
+    contador = contador + 1
+
+   
+    document.getElementById("vt").addEventListener("click", menosUm)
+    document.getElementById("av").addEventListener("click", maisUm)
+    if (contador < 0) {
+        contador = 0
+    }
+    var numlista = portuguesLista.length-1
+    if (contador > numlista) {
+        parar()
+    }
+    document.querySelector(".contando").innerHTML=`${contador}/${portuguesLista.length}`
+}
+
+
+function iniciar() {
+    intervalo = setInterval(lista, 1800)
+    document.getElementById('play').style.display='none'
+    document.getElementById('stop').style.display=''
+    document.getElementById('play').addEventListener('click',verificar)
+}
+
+
+function maisUm(){
+    contador=contador+1
+}
+function menosUm(){
+    contador=contador-2
+}
+
+
+
+function parar() {
+    clearInterval(intervalo)
+    document.getElementById('play').style.display=''
+    document.getElementById('stop').style.display='none'
+}
+
+
+function verificar (){
+    if(contador >= portuguesLista.length){
+        contador=0
+    }
+}
+
+
